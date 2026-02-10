94f614dd59e0392e9e85557be7f16eff4e6f13c2
Sergey Arsenyev <sergetester33@gmail.com>
2026-02-10 07:05:55 +0200

    added code

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -8,11 +8,18 @@
 */
 
 // тестовые данные (значения можно менять)
-const isAdmin = false
-const isVerifiedUser = true
+let isAdmin = false
+let isVerifiedUser = true
 const hasSpecialPermission = true
 const hasTemporaryPass = false
 
 let isAccess
 
 // your code
+isAccess =
+    (isAdmin || isVerifiedUser) &&
+    (hasSpecialPermission || hasTemporaryPass)
+
+
+
+
