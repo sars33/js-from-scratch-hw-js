e0b5f72e71218224fb750310a1be96aff5434bc0
Sergey Arsenyev <sergetester33@gmail.com>
2026-02-10 08:04:09 +0200

    added 02 code

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,11 +7,16 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
+let dice1 = Math.ceil(Math.random() * 6); // your code
+let dice2 = Math.ceil(Math.random() * 6);  // your code
 let isWinningDouble // your code
 
-/*
+if (dice1 === dice2 && (dice1 > 3)) {
+    isWinningDouble = true;
+} else {
+    isWinningDouble = false;
+}
+
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
 if (isWinningDouble) {
@@ -19,4 +24,4 @@ if (isWinningDouble) {
 } else {
   console.log('Не выигрышный дубль.')
 }
-*/
+
